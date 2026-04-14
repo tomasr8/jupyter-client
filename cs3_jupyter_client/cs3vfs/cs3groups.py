ab84e341c0e34de97eda190d220461e838352e7a
rwelande <rasmus.oscar.welander@cern.ch>
2026-04-14 17:14:22 +0200

    add get_groups call

--- cs3_jupyter_client/cs3vfs/cs3groups.py
+++ cs3_jupyter_client/cs3vfs/cs3groups.py
@@ -38,3 +38,18 @@ class CS3Groups:
         except Exception as e:
             self.status_handler.handle_errors(e)
             return []
+
+    @retry_on_auth_failure
+    def get_group(self, opaque_id: str, idp: str) -> dict:
+        """
+        Get information about a specific group by its ID and idp.
+        """
+        try:
+            result = self.client.group.get_group(
+                self.auth.get_token(),
+                opaque_id,
+                idp
+            )
+            return result if result is not None else {}
+        except Exception as e:
+            self.status_handler.handle_errors(e)
